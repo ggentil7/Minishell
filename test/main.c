93650c8bdfce4d63b4a534c06fefe51a855b5e6b
aboymond <aboymond@student.42lausanne.ch>
2022-09-16 13:08:50 +0200

    test

---
+++ test/main.c
@@ -0,0 +1,19 @@
+#include "../libft/libft.h"
+
+int main(int ac, char **av)
+{
+    void(ac);
+    check_s_c(av);
+    check_d_c(av);
+    return (0);
+}
+
+int check_s_c(char **av)
+{
+
+}
+
+int check_d_c(char **av)
+{
+
+}
