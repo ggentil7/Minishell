11185419a7f498e1261025fd3e172bdf6f4878ff
aboymond <aboymond@student.42lausanne.ch>
2022-09-19 09:55:19 +0200

    test

--- test/main.c
+++ test/main.c
@@ -7,6 +7,7 @@
 
 int	check_s_c(char *line);
 int	check_d_c(char *line);
+int	check_c(char *str);
 
 int	main(void)
 {
@@ -15,45 +16,77 @@ int	main(void)
 	while (42)
 	{
 		line = readline("test de merde : ");
-		printf("single return (%d)\n", check_s_c(line));
-		printf("double return (%d)\n", check_d_c(line));
+		check_c(line);
+		//printf("single return (%d)\n", check_s_c(line));
+		//printf("double return (%d)\n", check_d_c(line));
 	}
 	free (line);
 	return (0);
 }
 
-int	check_s_c(char *line)
+int	check_c(char *str)
 {
-	int	i;
-	int	compt;
+	int		i;
+	// int		j;
+	// int 	compt_i;
+	// int		compt_j;
+	int		compt;
 
 	i = 0;
+	// j = 0;
+	// compt_i = 0;
+	// compt_j = 0;
 	compt = 0;
-	while (line[i])
+	while (str[i])
 	{
-		if (line[i] == 39)
+		if (str[i] == 34)
+		{
+			i++;
 			compt++;
-		i++;
+			printf("compt = %d\n", compt);
+			while (str[i] != 34 || !str[i])
+			{
+				i++;
+			}
+		}
 	}
-	if (compt % 2 == 1)
-		return (1);
+	// if (compt % 2 == 1)
+	// 	return (1);
 	return (0);
 }
 
-int	check_d_c(char *line)
-{
-	int	i;
-	int	compt;
+// int	check_s_c(char *line)
+// {
+// 	int	i;
+// 	int	compt;
 
-	i = 0;
-	compt = 0;
-	while (line[i])
-	{
-		if (line[i] == 34)
-			compt++;
-		i++;
-	}
-	if (compt % 2 == 1)
-		return (1);
-	return (0);
-}
+// 	i = 0;
+// 	compt = 0;
+// 	while (line[i])
+// 	{
+// 		if (line[i] == 39)
+// 			compt++;
+// 		i++;
+// 	}
+// 	if (compt % 2 == 1)
+// 		return (1);
+// 	return (0);
+// }
+
+// int	check_d_c(char *line)
+// {
+// 	int	i;
+// 	int	compt;
+
+// 	i = 0;
+// 	compt = 0;
+// 	while (line[i])
+// 	{
+// 		if (line[i] == 34)
+// 			compt++;
+// 		i++;
+// 	}
+// 	if (compt % 2 == 1)
+// 		return (1);
+// 	return (0);
+// }
